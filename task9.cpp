49fae14511467bd8bb749033ba35ff2c78c9c341
yanyuw <32706350+yanyuw@users.noreply.github.com>
2018-11-19 23:14:13 +0800

    ds update task9

--- task9.cpp
+++ task9.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <windows.h>
 #include <stdio.h>
 #include <time.h>
+
 #define random(x,y) (x + rand()% (y-x))
 
 void* randomNumber(int* a, int n){
@@ -38,30 +40,50 @@ void insertSort(int a[], int n){
     }
 }
 
-void quickSort(int a[], int left, int right){
-    if(left > right){
-        return;
-    } else {
-        int i = left, j = right, tmp = a[left];
-        while(i != j){
-            for(; i < j && a[j] >= tmp; j--);
-            for(; i < j && a[i] <= tmp; i++);
-            if(i < j){
-                swap(&a[i], &a[j]);
-            }
+//void quickSort(int a[], int left, int right){
+//    if(left > right){
+//        return;
+//    } else {
+//        int i = left, j = right, tmp = a[left];
+//        while(i != j){
+//            for(; i < j && a[j] >= tmp; j--);
+//            for(; i < j && a[i] <= tmp; i++);
+//            if(i < j){
+//                swap(&a[i], &a[j]);
+//            }
+//        }
+//        a[left] = a[i];
+//        a[i] = tmp;
+//        quickSort(a, left, i-1);
+//        quickSort(a, i+1, right);
+//    }
+//}
+
+int partition(int a[], int left, int right){
+    int i = left;
+    for(int j = i+1; j <= right; j++){
+        if(a[left] > a[j]){
+            i++;
+            swap(&a[i], &a[j]);
         }
-        a[left] = a[i];
-        a[i] = tmp;
-        quickSort(a, left, i-1);
-        quickSort(a, i+1, right);
+    }
+    swap(&a[left], &a[i]);
+    return i;
+}
+
+void quickSort(int a[], int left, int right) {
+    if(left < right){
+        int pivet = partition(a, left, right);
+        quickSort(a, left, pivet-1);
+        quickSort(a, pivet+1, right);
     }
 }
 
-
 int main(){
     srand((unsigned)time(NULL));
-    double start, finish, duration;
-    int n = random(10, 50);//产生一个10～50间的数
+    DWORD start, finish;
+    double duration;
+    int n = random(10, 50000);//产生一个10～50000间的数
     int *a = (int *)malloc(sizeof(int) * n);
     randomNumber(a, n);//随机生成数组中的元素
     int b[n], c[n];
@@ -71,35 +93,36 @@ int main(){
     }
 
     //冒泡排序：
-    start = clock();  
+    start = GetTickCount();
     bubbleSort(a, n);
-    finish = clock();
-    duration = (finish - start) / CLOCKS_PER_SEC; 
+    finish = GetTickCount();
+    duration = (finish - start) / 1000.0;
     printf("bubbleSort running time: %lf s\n", duration);
     //插入排序：
-    start = clock();  
+    start = GetTickCount();
     insertSort(b, n);
-    finish = clock();
-    duration = (finish - start) / CLOCKS_PER_SEC; 
+    finish = GetTickCount();
+    duration = (finish - start) / 1000.0;
     printf("insertSort running time: %lf s\n", duration);
     //快速排序：
-    start = clock();  
+    start = GetTickCount();
     quickSort(c, 0, n-1);
-    finish = clock();
-    duration = (finish - start) / CLOCKS_PER_SEC; 
+    finish = GetTickCount();
+    duration = (finish - start) / 1000.0;
     printf("quickSort running time: %lf s\n", duration);
+/*
+     for(int i = 0; i < n; i++){
+         printf("%d ", a[i]);
+     }
+     printf("\n");
+     for(int i = 0; i < n; i++){
+         printf("%d ", b[i]);
+     }
 
-    // for(int i = 0; i < n; i++){
-    //     printf("%d ", a[i]);
-    // }
-    // printf("\n");
-    // for(int i = 0; i < n; i++){
-    //     printf("%d ", b[i]);
-    // }
-
-    // printf("\n");
-    //     for(int i = 0; i < n; i++){
-    //     printf("%d ", c[i]);
-    // }
+     printf("\n");
+         for(int i = 0; i < n; i++){
+         printf("%d ", c[i]);
+     }
+*/
     return 0;
 }
